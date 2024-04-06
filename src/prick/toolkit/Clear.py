194ff52271c9867933f590896c229f3b58b73468
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-04-07 00:36:44 +0200

    Create prick tools (#1)

---
+++ src/prick/toolkit/Clear.py
@@ -0,0 +1,4 @@
+import os
+
+def Auto():
+    os.system('cls' if os.name=='nt' else 'clear')
---
+++ src/prick/toolkit/wget_routes.py
@@ -0,0 +1,182 @@
+routes = [
+    {
+        "name": "Microsoft Visual Studio Code",
+        "url": "winget install -e --id Microsoft.VisualStudioCode",
+        "labels": ["programming", "code", "microsoft"]
+    },
+    {
+        "name": "Adobe Acrobat Reader 64bits",
+        "url": "winget install -e --id Adobe.Acrobat.Reader.64-bit",
+        "labels": ["pdf"]
+    },
+    {
+        "name": "Google Chrome",
+        "url": "winget install -e --id Google.Chrome",
+        "labels": ["google", "browsers"]
+    },
+    {
+        "name": "Mozilla Firefox",
+        "url": "winget install -e --id Mozilla.Firefox",
+        "labels": ["browsers", "recommended"]
+    },
+    {
+        "name": "LibreOffice",
+        "url": "winget install -e --id TheDocumentFoundation.LibreOffice",
+        "labels": ["office", "utils", "school"]
+    },
+    {
+        "name": "VLC",
+        "url": "winget install -e --id VideoLAN.VLC",
+        "labels": ["video", "audio", "movies"]
+    },
+    {
+        "name": "WinZIP",
+        "url": "winget install -e --id Corel.WinZip",
+        "labels": ["zip", "utils", "compression"]
+    },
+    {
+        "name": "OpenOffice",
+        "url": "winget install -e --id Apache.OpenOffice",
+        "labels": ["apache", "office", "utils"]
+    },
+    {
+        "name": "WinRAR",
+        "url": "winget install -e --id RARLab.WinRAR",
+        "labels": ["rar", "compression"]
+    },
+    {
+        "name": "CCleaner",
+        "url": "winget install -e --id Piriform.CCleaner",
+        "labels": ["security"]
+    },
+    {
+        "name": "Spotify",
+        "url": "winget install -e --id Spotify.Spotify",
+        "labels": ["music", "spotify"]
+    },
+    {
+        "name": "GIMP",
+        "url": "winget install -e --id GIMP.GIMP",
+        "labels": ["photo", "proffessional"]
+    },
+    {
+        "name": "OBS Studio",
+        "url": "winget install -e --id OBSProject.OBSStudio",
+        "labels": ["recording", "streaming", "proffessional"]
+    },
+    {
+        "name": "Steam",
+        "url": "winget install -e --id Valve.Steam",
+        "labels": ["valve", "steam", "games", "videogames", "gamming", "shop"]
+    },
+    {
+        "name": "Zoom",
+        "url": "winget install -e --id Zoom.Zoom",
+        "labels": ["meetings", "chat", "videochat", "social"]
+    },
+    {
+        "name": "KeePass",
+        "url": "winget install -e --id DominikReichl.KeePass",
+        "labels": ["passwords", "security", "recommended"]
+    },
+    {
+        "name": "LastPass",
+        "url": "winget install -e --id LogMeIn.LastPass",
+        "labels": ["passwords", "security"]
+    },
+    {
+        "name": "PowerToys",
+        "url": "winget install -e --id Microsoft.PowerToys",
+        "labels": ["microsoft", "utils", "recommended", "office", "school"]
+    },
+    {
+        "name": "NodeJS",
+        "url": "winget install -e --id OpenJS.NodeJS",
+        "labels": ["node", "programming", "javascript"]
+    },
+    {
+        "name": "Git",
+        "url": "winget install -e --id Git.Git",
+        "labels": ["git", "repositories", "versions"]
+    },
+    {
+        "name": "GitHub Desktop",
+        "url": "winget install -e --id GitHub.GitHubDesktop",
+        "labels": ["git", "github", "repositories", "versions"]
+    },
+    {
+        "name": "7ZIP",
+        "url": "winget install -e --id 7zip.7zip",
+        "labels": ["zip", "compression", "office", "school", "utils"]
+    },
+    {
+        "name": "Brackets",
+        "url": "winget install -e --id Adobe.Brackets",
+        "labels": ["code", "programming", "starters"]
+    },
+    {
+        "name": "Amazon Games",
+        "url": "winget install -e --id Amazon.Games",
+        "labels": ["amazon", "games", "videogames", "gamming", "shop"]
+    },
+    {
+        "name": "AnyDesk",
+        "url": "winget install -e --id AnyDeskSoftwareGmbH.AnyDesk",
+        "labels": ["anydesk", "control", "remote", "utils", "office"]
+    },
+    {
+        "name": "XAMPP",
+        "url": "winget install -e --id ApacheFriends.Xampp.8.2",
+        "labels": ["apache", "programming"]
+    },
+    {
+        "name": "Arduino",
+        "url": "winget install -e --id ArduinoSA.IDE.stable",
+        "labels": ["arduino", "programming", "robotics"]
+    },
+    {
+        "name": "Audacity",
+        "url": "winget install -e --id Audacity.Audacity",
+        "labels": ["audio", "proffessional"]
+    },
+    {
+        "name": "Balena Etcher",
+        "url": "winget install -e --id Balena.Etcher",
+        "labels": ["balena", "flasher"]
+    },
+    {
+        "name": "Birvise SSH Client",
+        "url": "winget install -e --id Bitvise.SSH.Client",
+        "labels": ["ssh", "bitvise", "servers"]
+    },
+    {
+        "name": "Blender",
+        "url": "winget install -e --id BlenderFoundation.Blender",
+        "labels": ["animation", "modelation", "proffessional"]
+    },
+    {
+        "name": "Blue Stacks",
+        "url": "winget install -e --id BlueStack.BlueStacks",
+        "labels": ["emulator", "android"]
+    },
+    {
+        "name": "Brave",
+        "url": "winget install -e --id Brave.Brave",
+        "labels": ["browser"]
+    },
+    {
+        "name": "Ubuntu 22.04",
+        "url": "winget install -e --id Canonical.Ubuntu.2204",
+        "labels": ["ubuntu", "terminal", "linux"]
+    },
+    {
+        "name": "Webex",
+        "url": "winget install -e --id Cisco.WebexTeams",
+        "labels": ["cisco", "meetings", "social", "office"]
+    },
+    {
+        "name": "Corsair iCue 4",
+        "url": "winget install -e --id Corsair.iCUE.4",
+        "labels": ["corsair", "personalization"]
+    }
+]
