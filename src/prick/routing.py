f532de19b1a04afd2c19469493bc26269c6fc5df
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-04-07 00:37:20 +0200

    Create prick routing (#1)

---
+++ src/prick/routing.py
@@ -0,0 +1,37 @@
+from .toolkit import Language, Font
+
+trans = Language.Translation.Translate_Language
+tcolor = Font.Color.Text
+bcolor = Font.Color.Back
+filename = Language.Translation.Get_Language()
+filename
+error404header = f"!:{tcolor.RED}\n\n{" "*5}{bcolor.RED}{tcolor.BLACK}{" "*15}4 0 4{" "*15}{bcolor.RESET}{tcolor.RESET}\n\n{" "*10}{trans(filename, "Menu", "error404message")}\n\n\n{tcolor.RESET}"
+defaulttablehead = f"!:{bcolor.WHITE}{tcolor.BLACK}\n {trans(filename, "Menu", "headerdefault")}{" "*18}\n{bcolor.RESET}{tcolor.RESET}"
+errortablehead = f"!:{bcolor.WHITE}{tcolor.BLACK}\n {trans(filename, "Menu", "errordefault")}{" "*18}\n{bcolor.RESET}{tcolor.RESET}"
+
+class Structure:
+    class Home:
+        Tablehead = defaulttablehead
+        Exit = "?return"
+        DeviceDiagnosis = "?menu:Diagnosis"
+        FileManager = "?menu:Filemanager"
+        Configuration = "?help:config"
+    class E404:
+        header = error404header
+        Tablehead = errortablehead
+        Exit = "?return"
+        Home = "?menu:Home"
+    class Diagnosis:
+        Tablehead = defaulttablehead
+        Return = "?return"
+        Get_Data = "?page:dg_data"
+        Help = "?help:dg_help"
+    class Filemanager:
+        Tablehead = defaulttablehead
+        Return = "?return"
+        By_FirstLetter = "?page:fm_letter"
+        By_FirstWord = "?page:fm_word"
+        By_FirstExtension = "?page:fm_extension"
+        Help = "?help:fm_help"
+    home = "Home"
+    notfound = "E404"
